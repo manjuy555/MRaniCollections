c3df3de60dc1c7110cfc810020c3afa81d8fed8f
Manju Yadavar <myadavar@optebiz.com>
2026-03-06 18:07:49 +0530

    latest update to launch

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+            base: "/MRaniCollections/",
+
   plugins: [
     react({
-          base: "/MRaniCollections/",
 
       babel: {
         plugins: [['babel-plugin-react-compiler']],
